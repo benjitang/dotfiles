3de8c7f984cfadd1ef256ed26ef7023304a0979b
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-07-03 11:12:02 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:block-blinkwait700-blinkoff400-blinkon250"
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+
+require("core.options")
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
