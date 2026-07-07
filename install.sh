5defbd1ebd46de2a6cb1e661a81d7b9f44c08b2f
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-07-06 20:54:31 -0400

    Add tmux config

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p ~/.config
 ln -sfn "$DOTFILES/nvim" ~/.config/nvim
 ln -sfn "$DOTFILES/starship.toml" ~/.config/starship.toml
+ln -sfn "$DOTFILES/.tmux.conf" ~/.tmux.conf
 
 echo "nvim config linked from $DOTFILES/nvim"
 echo "starship config linked from $DOTFILES/starship.toml"
+echo "tmux config linked from $DOTFILES/.tmux.conf"
