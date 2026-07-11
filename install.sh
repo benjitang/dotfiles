57242fc90526e223125402a367efa5700b845183
Benjamin Tang Desktop <benjamin.code.tang@gmail.com>
2026-07-10 21:11:53 -0400

    Add zsh file

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ mkdir -p ~/.config
 ln -sfn "$DOTFILES/nvim" ~/.config/nvim
 ln -sfn "$DOTFILES/starship/starship.toml" ~/.config/starship.toml
 ln -sfn "$DOTFILES/tmux/.tmux.conf" ~/.tmux.conf
+ln -sfn "$DOTFILES/zsh/.zshrc" ~/.zshrc
 
 echo "nvim config linked from $DOTFILES/nvim"
 echo "starship config linked from $DOTFILES/starship.toml"
 echo "tmux config linked from $DOTFILES/.tmux.conf"
+echo "zsh config linked from $DOTFILES/zsh/.zshrc"
